f5971564e0920d97ad7a76915adf0868306bb4a9
islamMaruf <marufislam7424@gmail.com>
2020-04-11 00:02:31 +0600

    user route added to serve.js

--- server.js
+++ server.js
@@ -10,34 +10,37 @@ app.use(cors());
 //see the request log to the console
 app.use(morgan('dev'));
 // add body to the post method
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
 //middleware
 app.use((req, res, next) => {
     console.log('I am a middleware function');
     next();
 })
 
-mongoose.connect('mongodb://localhost:27017/contacts',{'useNewUrlParser':true});
+mongoose.connect('mongodb://localhost:27017/contacts', {'useNewUrlParser': true});
 const db = mongoose.connection;
-db.on('error',function (err) {
+db.on('error', function (err) {
     console.log(err);
 })
 
-db.once('open',() => (console.log('db open')));
+db.once('open', () => (console.log('db open')));
 
 const contactRoute = require('./routes/contact');
- app.use('/api/contacts/',contactRoute)
+const userRoute = require('./routes/user');
+app.use('/api/contacts/', contactRoute)
+app.use('api/users/',userRoute)
 
-app.get('/', (req,res) => {
-    res.send('<h1>Hello</h1>')
+
+app.get('/', (req, res) => {
+    res.send('welcome to contact api')
 })
-
 
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port} `);
-    
+
 })
 
 
