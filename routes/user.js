697c0a2d5c40ebbde682894de39a882adcc05ce8
islamMaruf <marufislam7424@gmail.com>
2020-04-11 02:05:39 +0600

    middleware added

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
-
+const authenticate = require('../middleware/authenticate')
 router.post('/login', userController.login);
 router.post('/register', userController.registration);
-router.get('/',userController.allUser);
+router.get('/',authenticate,userController.allUser);
 
 module.exports = router
